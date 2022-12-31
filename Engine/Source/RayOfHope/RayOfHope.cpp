fc51f418089a47952a9e09e3b255785f18197d1f
bassel97 <bassel.ahmed97@eng-st.cu.edu.eg>
2023-01-01 01:03:45 +0200

    Added engine code

---
+++ Engine/Source/RayOfHope/RayOfHope.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "RayOfHope.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RayOfHope, "RayOfHope" );
