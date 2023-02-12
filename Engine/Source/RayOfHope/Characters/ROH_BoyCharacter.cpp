b57576b84bd88f2789c927b99f310a46664c83d6
bassel97 <bassel.ahmed97@eng-st.cu.edu.eg>
2023-02-12 18:44:21 +0200

    Added character movement logic and animations

--- Engine/Source/RayOfHope/Characters/ROH_BoyCharacter.cpp
+++ Engine/Source/RayOfHope/Characters/ROH_BoyCharacter.cpp
@@ -2,8 +2,12 @@
 
 #include "ROH_BoyCharacter.h"
 #include "Components/CapsuleComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "RayOfHope/Pawns/ROH_HopePawn.h"
 
 AROH_BoyCharacter::AROH_BoyCharacter()
 {
@@ -25,12 +29,87 @@ void AROH_BoyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	// Update Hope position
+	if (CreatedHopeRefrence)
+	{
+		const FVector2D hopeDirection = FVector2D(HopeHorizontalInput, HopeVerticalInput);
+		CreatedHopeRefrence->SetPlayerInput(hopeDirection);
+	}
+
+	// Update Boy's navigation sensors
+	UpdateSensor(1.0f);
+	UpdateSensor(-1.0f);
+
+	// Boy Jumping up
+	if (!GetMovementComponent()->IsMovingOnGround())
+	{
+		if (bIsJumpingRight)
+		{
+			AddMovementInput(FVector(1.0, 0.0f, 0.0f));
+		}
+		if (bIsJumpingLeft)
+		{
+			AddMovementInput(FVector(-1.0f, 0.0f, 0.0f));
+		}
+	}
+	else
+	{
+		if (bIsJumpingRight)
+		{
+			bIsJumpingRight = false;
+		}
+		if (bIsJumpingLeft)
+		{
+			bIsJumpingLeft = false;
+		}
+	}
+
+	// Boy Jumping Down
+	if (bIsJumpingDownRight)
+	{
+		if (!bCanMoveRight)
+		{
+			AddMovementInput(FVector(1.0f, 0.0f, 0.0f));
+		}
+		else
+		{
+			bIsJumpingDownRight = false;
+		}
+	}
+	if (bIsJumpingDownLeft)
+	{
+		if (!bCanMoveLeft)
+		{
+			AddMovementInput(FVector(-1.0f, 0.0f, 0.0f));
+		}
+		else
+		{
+			bIsJumpingDownLeft = false;
+		}
+	}
+
+	if (bIsPushingPulling)
+	{
+		if (ActorToPush)
+		{
+			ActorToPush->SetActorLocation(GetActorLocation() + CapturedPushObjectOffset);
+		}
+	}
 }
 
 void AROH_BoyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAction("BoyClimb", IE_Pressed, this, &AROH_BoyCharacter::BoyTryClimb);
+	PlayerInputComponent->BindAction("BoyJumpDown", IE_Pressed, this, &AROH_BoyCharacter::BoyTryJumpDown);
+
+	PlayerInputComponent->BindAction("BoyPush", IE_Pressed, this, &AROH_BoyCharacter::BoyStartPush);
+	PlayerInputComponent->BindAction("BoyPush", IE_Released, this, &AROH_BoyCharacter::BoyEndPush);
+
+	PlayerInputComponent->BindAxis("HopeMoveHorizontal", this, &AROH_BoyCharacter::HopeMoveHorizontal);
+	PlayerInputComponent->BindAxis("HopeMoveVertical", this, &AROH_BoyCharacter::HopeMoveVertical);
+	PlayerInputComponent->BindAxis("BoyMoveHorizontal", this, &AROH_BoyCharacter::BoyMoveHorizontal);
 }
 
 void AROH_BoyCharacter::UpdateCameraOptions()
@@ -40,6 +119,118 @@ void AROH_BoyCharacter::UpdateCameraOptions()
 		SpringArmComponent->TargetArmLength = SpringLength;
 		SpringArmComponent->SocketOffset = SpringArmOffset;
 		SpringArmComponent->SetRelativeRotation(SpringArmRotator);
+	}
+}
+
+void AROH_BoyCharacter::BoyTryClimb()
+{
+	if (bIsPushingPulling)
+	{
+		return;
+	}
+
+	if (bCanJumpRight && BoyHorizontalInput > 0.0f)
+	{
+		bIsJumpingRight = true;
+		Jump();
+	}
+
+	if (bCanJumpLeft && BoyHorizontalInput < 0.0f)
+	{
+		bIsJumpingLeft = true;
+		Jump();
+	}
+}
+void AROH_BoyCharacter::BoyTryJumpDown()
+{
+	if (bIsPushingPulling)
+	{
+		return;
+	}
+
+	if (!bCanMoveRight)
+	{
+		bIsJumpingDownRight = true;
+	}
+	if (!bCanMoveLeft)
+	{
+		bIsJumpingDownLeft = true;
+	}
+}
+
+void AROH_BoyCharacter::BoyStartPush()
+{
+	if (bIsJumpingRight || bIsJumpingLeft || bIsJumpingDownRight || bIsJumpingDownLeft)
+	{
+		return;
+	}
+
+	if (bCanPushBoxRight || bCanPushBoxLeft)
+	{
+		Crouch();
+		bIsPushingPulling = true;
+		CapturedPushObjectOffset = ActorToPush->GetActorLocation() - GetActorLocation();
+		if (ActorToPush)
+		{
+			if (UPrimitiveComponent* rootComp = Cast<UPrimitiveComponent>(ActorToPush->GetRootComponent()))
+			{
+				rootComp->SetSimulatePhysics(false);
+				rootComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+			}
+		}
+		if (UCharacterMovementComponent* const MovementComponent = GetCharacterMovement())
+		{
+			MovementComponent->bOrientRotationToMovement = false;
+		}
+	}		
+}
+void AROH_BoyCharacter::BoyEndPush()
+{
+	if (bIsJumpingRight || bIsJumpingLeft || bIsJumpingDownRight || bIsJumpingDownLeft)
+	{
+		return;
+	}
+
+	if (bIsPushingPulling)
+	{
+		UnCrouch();
+		bIsPushingPulling = false;
+		if (ActorToPush)
+		{
+			if (UPrimitiveComponent* rootComp = Cast<UPrimitiveComponent>(ActorToPush->GetRootComponent()))
+			{
+				rootComp->SetSimulatePhysics(true);
+				rootComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+			}
+		}
+		CapturedPushObjectOffset = FVector();
+		if (UCharacterMovementComponent* const MovementComponent = GetCharacterMovement())
+		{
+			MovementComponent->bOrientRotationToMovement = true;
+		}
+	}
+}
+
+void AROH_BoyCharacter::HopeMoveHorizontal(float axisValue)
+{
+	HopeHorizontalInput = axisValue;
+}
+void AROH_BoyCharacter::HopeMoveVertical(float axisValue)
+{
+	HopeVerticalInput = axisValue;
+}
+void AROH_BoyCharacter::BoyMoveHorizontal(float axisValue)
+{
+	if (bIsJumpingRight || bIsJumpingLeft || bIsJumpingDownRight || bIsJumpingDownLeft)
+	{
+		return;
+	}
+
+	BoyHorizontalInput = axisValue;
+
+	if ((axisValue > 0 && bCanMoveRight) || (axisValue < 0 && bCanMoveLeft))
+	{
+		AddMovementInput(FVector(axisValue, 0.0f, 0.0f));
 	}
 }
 
@@ -68,6 +259,10 @@ void AROH_BoyCharacter::BeginPlay()
 	
 	UpdateHeight();
 	UpdateCameraOptions();
+
+	CreatedHopeRefrence = GetWorld()->SpawnActor<AROH_HopePawn>(
+		HopePawnToSpawn, GetActorLocation() + FVector(200.0f, 0, 200.0f), GetActorRotation()
+		);
 }
 
 void AROH_BoyCharacter::UpdateHeight()
@@ -84,5 +279,95 @@ void AROH_BoyCharacter::UpdateHeight()
 	if (USkeletalMeshComponent* characterMesh = GetMesh())
 	{
 		characterMesh->SetRelativeRotation(FRotator(0, 270, 0));
+	}
+}
+
+void AROH_BoyCharacter::UpdateSensor(float sign)
+{
+	//TODO: Replace with actual distances or calculate from cylinder raduis
+	const FVector startPosition = GetActorLocation() + FVector(sign * 35.0f, 0.0f, RayCastPosZOffset);
+	const FVector endPosition = GetActorLocation() + FVector(sign * 35.0f, 0.0f, -RayCastNegZOffset);
+
+	FHitResult outHit;
+	const bool hit = UKismetSystemLibrary::SphereTraceSingleForObjects(
+		GetWorld(),
+		startPosition,
+		endPosition,
+		10.0f,
+		ObjectTypesArray,
+		false,
+		{ },
+		EDrawDebugTrace::ForOneFrame,
+		outHit,
+		true
+	);
+
+	const bool isFacingObject = (outHit.ImpactPoint - GetActorLocation()).Dot(GetActorForwardVector()) > 0.85f;
+	const bool isObjectInfront = FMath::Abs(outHit.ImpactPoint.Z - endPosition.Z) > 25.0f;
+	if (hit)
+	{
+		// right
+		if (sign > 0)
+		{
+			bCanMoveRight = true;
+			if (isObjectInfront)
+			{
+				bCanJumpRight = true;
+				JumpTargetRight = outHit.ImpactPoint + BoyHeight / 2.0f;
+			}
+			if (isObjectInfront && isFacingObject)
+			{
+				bCanPushBoxRight = true;
+				ActorToPush = outHit.GetActor();
+			}
+			else
+			{
+				bCanPushBoxRight = false;
+			}
+		}
+		// left
+		else if (sign < 0)
+		{
+			bCanMoveLeft = true;
+			if (isObjectInfront)
+			{
+				bCanJumpLeft = true;
+				JumpTargetLeft = outHit.ImpactPoint + BoyHeight / 2.0f;
+			}
+			if (isObjectInfront && isFacingObject)
+			{
+				bCanPushBoxLeft = true;
+				ActorToPush = outHit.GetActor();
+			}
+			else
+			{
+				bCanPushBoxLeft = false;
+			}
+		}
+	}
+	else
+	{
+		// right
+		if (sign > 0)
+		{
+			bCanMoveRight = false;
+			if (isObjectInfront)
+			{
+				bCanJumpRight = false;
+			}
+		}
+		// left
+		else if (sign < 0)
+		{
+			bCanMoveLeft = false;
+			if (isObjectInfront)
+			{
+				bCanJumpLeft = false;
+			}
+		}
+
+		bCanPushBoxRight = false;
+		bCanPushBoxLeft = false;
+		ActorToPush = nullptr;		
 	}
 }
--- Engine/Source/RayOfHope/Characters/ROH_BoyCharacter.h
+++ Engine/Source/RayOfHope/Characters/ROH_BoyCharacter.h
@@ -20,6 +20,26 @@ public:
 	
 	void UpdateCameraOptions();
 
+	// Input Functions:
+	void BoyTryClimb();
+	void BoyTryJumpDown();
+
+	void BoyStartPush();
+	void BoyEndPush();
+
+	void HopeMoveHorizontal(float axisValue);
+	void HopeMoveVertical(float axisValue);
+	void BoyMoveHorizontal(float axisValue);
+
+	UFUNCTION(BlueprintCallable, Category = Boy)
+	bool IsBoyJumping() const {	return bIsJumpingRight || bIsJumpingLeft; }
+
+	UFUNCTION(BlueprintCallable, Category = Boy)
+	bool IsBoyJumpingDown() const { return bIsJumpingDownRight || bIsJumpingDownLeft; }
+
+	UFUNCTION(BlueprintCallable, Category = Boy)
+	bool IsBoyPushingPulling() const { return bIsPushingPulling; }
+
 #if WITH_EDITOR
 	virtual void PostEditChangeProperty(FPropertyChangedEvent& PropertyChangedEvent) override;
 #endif
@@ -29,7 +49,41 @@ protected:
 
 	void UpdateHeight();
 
+	//TODO: this function should get the sign and a refrence out to the struct
+	void UpdateSensor(float sign);
+
 protected:
+	class AROH_HopePawn* CreatedHopeRefrence = nullptr;
+	
+	// Boy movement variables**************************************
+	float HopeHorizontalInput = 0.0f;
+	float HopeVerticalInput = 0.0f;
+	float BoyHorizontalInput = 0.0f;
+
+	bool bIsJumpingRight = false;
+	bool bIsJumpingLeft = false;
+
+	bool bIsJumpingDownRight = false;
+	bool bIsJumpingDownLeft = false;
+
+	bool bIsPushingPulling = false;
+	FVector CapturedPushObjectOffset = FVector();
+
+	//TODO: Turn into struct
+	bool bCanMoveRight = false;
+	bool bCanJumpRight = false;
+	bool bCanPushBoxRight = false;
+	bool bCanPushBoxLeft = false;
+	FVector JumpTargetRight = FVector();
+	AActor* ActorToPush = nullptr;
+
+	bool bCanMoveLeft = false;
+	//bool bCanPushBoxLeft = false;
+	bool bCanJumpLeft = false;
+	FVector JumpTargetLeft = FVector();
+	//AActor* ActorToPushLeft = nullptr;
+	/***********************************************************/
+
 	UPROPERTY(Category = Boy, EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	float BoyHeight = 140.0f;
 
@@ -45,12 +99,25 @@ protected:
 	UPROPERTY(Category = Boy, EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	float SpringLength = 600.0f;
 
+	UPROPERTY(Category = Boy, EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class AROH_HopePawn> HopePawnToSpawn = nullptr;
+
+
+	UPROPERTY(Category = BoyNavigation, EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RayCastPosZOffset = 10.0f;
+
+	UPROPERTY(Category = BoyNavigation, EditAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	float RayCastNegZOffset = 80.0f;
+
+	UPROPERTY(Category = BoyNavigation, EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	TArray<TEnumAsByte<EObjectTypeQuery>> ObjectTypesArray = TArray<TEnumAsByte<EObjectTypeQuery>>();
+
+
 private:
 	UPROPERTY(Category = Boy, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class USpringArmComponent> SpringArmComponent;
 
 	UPROPERTY(Category = Boy, VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	TObjectPtr<class UCameraComponent> CameraComponent;
-
 
 };
